1ede8238e39fef305bba2ce19105a7a176baaafb
RF <141957714+Thynevir@users.noreply.github.com>
2023-09-23 19:28:23 -0400

    More Practice 7
    I overcomplicated/overthought the solution for this one. Could be done in a single line rather than a few.

---
+++ practice/practice/DNAtoRNA.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class DNAtoRNA {
+
+	static String dna = "GCAT";
+	//TTTT
+
+	public static void main(String[] args) {
+		// 
+		
+		System.out.print(dnaToRna());
+
+	}
+	public static String dnaToRna() {
+		String rna = "";
+	      
+	      if(!dna.contains("T")){
+	        
+	        return dna;
+	      }
+	      else if(dna.contains("T")){
+	        
+	        rna = dna.replaceAll("T", "U");
+	      }
+	      
+	      return rna;
+	    } 
+	}
+
